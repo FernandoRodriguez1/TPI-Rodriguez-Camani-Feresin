ff1baf22b34f89043ed263e08191ce5930bf294a
SantiagoFeresin <santiagoferesin@gmail.com>
2024-04-02 17:03:00 -0300

    BrowserRouter instead of Router. Relocation of components.

--- tpi-final/src/App.js
+++ tpi-final/src/App.js
@@ -1,22 +1,55 @@
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import NavBar from "./Components/NavBar/NavBar";
-import Dashboard from "./Components/Dashboard/Dashboard";
 import Footer from "./Components/Footer/Footer";
+import Error404 from "./Components/Pages/Error404";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
 
-import Routess from "./Components/Routess";
+const App = () => {
+  const router = createBrowserRouter([
+    { path: "/home", element: <Navigate to="/" replace /> },
+    { path: "*", element: <Error404 /> },
+    {
+      path: "/",
+      element: (
+        <>
+          <NavBar />
+          <Dashboard />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/login",
+      element: (
+        <>
+          <NavBar />
+          <Login />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/register",
+      element: (
+        <>
+          <NavBar />
+          <Register />
+          <Footer />
+        </>
+      ),
+    },
+  ]);
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <div className="content">
-        <Dashboard className="flex-grow-1"></Dashboard>
-      </div>
-      <Footer />
-      <Routess />
-    </div>
-  );
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App;
